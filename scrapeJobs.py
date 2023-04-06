668efd0efce3f29c4537e98173e0d6427b9ff568
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2023-04-06 13:00:41 +0200

    saved job ids localy

--- scrapeJobs.py
+++ scrapeJobs.py
@@ -16,6 +16,8 @@ import user_input_validations as uiv
 import df_to_email
 import random
 import localcred
+import re
+import json
 
 """
 localcred file needs :
@@ -24,6 +26,22 @@ email_pword ()
 API_KEY ()
 
 """
+#returns list
+def load_prev_ids():
+     # Load previously scraped job ids from a local file (if available)
+    try:
+        with open('saved_ids.json', 'r') as file:
+            scraped_job_ids = json.load(file)
+    except FileNotFoundError:
+        scraped_job_ids = []
+    return scraped_job_ids
+
+
+def save_ids_localy(ids_lst):   
+    print(ids_lst)
+    with open('saved_ids.json', 'w') as f:
+        json.dump(ids_lst, f)
+    
 
 
 def randomize_move(browser):
@@ -34,7 +52,7 @@ def randomize_move(browser):
     random_number = str(random_number)
     browser.execute_script(f"window.scrollTo(10, {random_number})")
 
-
+#check if data is save localy, efficiently iterate n populate new data w/ old/new data
 # TODO: Get job ID from URLs.
 # TODO: Add job level detail.
 
@@ -111,7 +129,7 @@ if logging_in == "y":
 page = (num_of_jobs - 1) // 25 + 1
 
 job_links = []
-# Create a new instance of the Firefox driver
+# Create a new instance of the Chrome driver
 with webdriver.Chrome() as browser:
     browser.get("https://www.linkedin.com")
     try:
@@ -136,7 +154,7 @@ with webdriver.Chrome() as browser:
         # Find the element with the class "jobs-search-results-list"
         element = browser.find_element(By.CLASS_NAME, "jobs-search-results-list")
 
-        # Scroll the element down by the specified amount
+        # Scroll the element down by the specified amount (to load all jobs)
         for i in range(5):  # Scroll down 10 times
             browser.execute_script(f"arguments[0].scrollBy(0, {800});", element)
             time.sleep(2)
@@ -169,127 +187,136 @@ with webdriver.Chrome() as browser:
         time.sleep(2)
         url = browser.current_url
 
-    # quit()
     # get number of items request (shorten list if necessary)
     job_links = job_links[:num_of_jobs]
     # print(job_links)
     time.sleep(2)
     randomize_move(browser)
 
-    """ Now that we have all links from first page,
+    prev_ids=load_prev_ids()
+    
+    """ Now that we have all links from pages,
         iterate each link and gather information for each """
 
     tmp = 0
     data = []
-    for job in tqdm(job_links):
-        randomize_move(browser)
-        # tuple contains individual info for each job post
-        data_tup = ()
-        # get URL
-        indv_url = job
-        """ Navigate to each website and extract data"""
-        browser.get(indv_url)  # navigate to URL
-        time.sleep(2)
-        # press button to display Skills section
-        # Some profile have or dont have this section, test here
-        try:
-            wait = WebDriverWait(browser, 8)
-            button = wait.until(
-                EC.visibility_of_element_located(
-                    (By.CLASS_NAME, "jobs-unified-top-card__job-insight-text-button")
-                )
-            )
-            button = browser.find_element(
-                By.XPATH,
-                "//button[@class='jobs-unified-top-card__job-insight-text-button']",
-            ).click()
+    to_prev_ids_lst=[]
+    for cur_job in tqdm(job_links):
+        #
+        cur_job_id=re.search(r"/(\d+)/", cur_job).group(1)
+        print("cur:",cur_job_id,"prev" ,prev_ids)
+        if cur_job_id not in prev_ids:           
+            randomize_move(browser)
+            # tuple contains individual info for each job post
+            data_tup = ()
+            # get URL
+            indv_url = cur_job
+            """ Navigate to each website and extract data"""
+            browser.get(indv_url)  # navigate to URL
             time.sleep(2)
-        except:
-            pass
+            # press button to display Skills section
+            # Some profile have or dont have this section, test here
+            try:
+                wait = WebDriverWait(browser, 8)
+                button = wait.until(
+                    EC.visibility_of_element_located(
+                        (By.CLASS_NAME, "jobs-unified-top-card__job-insight-text-button")
+                    )
+                )
+                button = browser.find_element(
+                    By.XPATH,
+                    "//button[@class='jobs-unified-top-card__job-insight-text-button']",
+                ).click()
+                time.sleep(2)
+            except:
+                pass
 
-        content = browser.page_source
-        time.sleep(2)
-        soup = BeautifulSoup(content, "html.parser")
-        # Now that we general content , need to parse for specific details
-        title = soup.find(
-            "h1",
-            class_="t-24 t-bold jobs-unified-top-card__job-title",
-        ).text.strip()
-        data_tup = data_tup + (title,)
+            content = browser.page_source
+            time.sleep(2)
+            soup = BeautifulSoup(content, "html.parser")
+            # Now that we general content , need to parse for specific details
+            title = soup.find(
+                "h1",
+                class_="t-24 t-bold jobs-unified-top-card__job-title",
+            ).text.strip()
+            data_tup = data_tup + (title,)
 
-        company = soup.find(
-            "span",
-            class_="jobs-unified-top-card__company-name",
-        ).text.strip()
-        data_tup = data_tup + (company,)
+            company = soup.find(
+                "span",
+                class_="jobs-unified-top-card__company-name",
+            ).text.strip()
+            data_tup = data_tup + (company,)
 
-        location = soup.find(
-            "span",
-            class_="jobs-unified-top-card__bullet",
-        ).text.strip()
-        data_tup = data_tup + (location,)
+            location = soup.find(
+                "span",
+                class_="jobs-unified-top-card__bullet",
+            ).text.strip()
+            data_tup = data_tup + (location,)
 
-        workplace_type = soup.find(
-            "span",
-            class_="jobs-unified-top-card__workplace-type",
-        )
-        if workplace_type is not None:
-            workplace_type = workplace_type.text.strip()
-            data_tup = data_tup + (workplace_type,)
-        else:
-            data_tup = data_tup + ("No work place info",)
+            workplace_type = soup.find(
+                "span",
+                class_="jobs-unified-top-card__workplace-type",
+            )
+            if workplace_type is not None:
+                workplace_type = workplace_type.text.strip()
+                data_tup = data_tup + (workplace_type,)
+            else:
+                data_tup = data_tup + ("No work place info",)
 
-        date_posted = soup.find(
-            "span",
-            class_="jobs-unified-top-card__posted-date",
-        ).text.strip()
-        data_tup = data_tup + (date_posted,)
+            date_posted = soup.find(
+                "span",
+                class_="jobs-unified-top-card__posted-date",
+            ).text.strip()
+            data_tup = data_tup + (date_posted,)
 
-        # Skills: get <ul> that has <li> containing all skills
-        ul = soup.find("ul", {"class": "job-details-skill-match-status-list"})
-        if ul is not None:
-            # now that I have <li> with skills,strip unnecessary chars
-            # remove "add" from results
-            skills_list = [
-                li.text.strip().replace("Add", "") for li in ul.find_all("li")
-            ]
-            # remove /n and spaces from results
-            cleaned_list = [item.strip() for item in skills_list]
-            # create a readable string
-            skills_result = ", ".join([item.strip() for item in cleaned_list])
-            data_tup = data_tup + (skills_result,)
-        else:
-            data_tup = data_tup + ("No skills section found.",)
+            # Skills: get <ul> that has <li> containing all skills
+            ul = soup.find("ul", {"class": "job-details-skill-match-status-list"})
+            if ul is not None:
+                # now that I have <li> with skills,strip unnecessary chars
+                # remove "add" from results
+                skills_list = [
+                    li.text.strip().replace("Add", "") for li in ul.find_all("li")
+                ]
+                # remove /n and spaces from results
+                cleaned_list = [item.strip() for item in skills_list]
+                # create a readable string
+                skills_result = ", ".join([item.strip() for item in cleaned_list])
+                data_tup = data_tup + (skills_result,)
+            else:
+                data_tup = data_tup + ("No skills section found.",)
 
-        # retrieve skills macthed
-        matched_skills = soup.find("h4", {"class": "t-bold t-16"})
-        if matched_skills is not None:
-            matched_skills = matched_skills.text.strip()
-            data_tup = data_tup + (matched_skills,)
-        else:
-            data_tup = data_tup + ("No matched skills",)
+            # retrieve skills macthed
+            matched_skills = soup.find("h4", {"class": "t-bold t-16"})
+            if matched_skills is not None:
+                matched_skills = matched_skills.text.strip()
+                data_tup = data_tup + (matched_skills,)
+            else:
+                data_tup = data_tup + ("No matched skills",)
 
-        main_details = soup.find(
-            "div",
-            class_="jobs-box__html-content jobs-description-content__text t-14 t-normal jobs-description-content__text--stretch",
-        ).text.strip()
+            main_details = soup.find(
+                "div",
+                class_="jobs-box__html-content jobs-description-content__text t-14 t-normal jobs-description-content__text--stretch",
+            ).text.strip()
 
-        # clean \n
-        main_details = main_details.strip()
-        data_tup = data_tup + (main_details,)
-        # add link at last column
-        data_tup = data_tup + (indv_url,)
+            # clean \n
+            main_details = main_details.strip()
+            data_tup = data_tup + (main_details,)
+            # add link at last column
+            data_tup = data_tup + (indv_url,)
 
-        randomize_move(browser)
+            randomize_move(browser)
 
-        # add all data into data frame
-        data.append(data_tup)
-        time.sleep(2)
+            # add all data into data frame
+            data.append(data_tup)
+            time.sleep(2)
 
-        tmp += 1
-        if tmp == num_of_jobs:
-            break
-
+            #append new added id to prev ids list
+            to_prev_ids_lst.append(cur_job_id)
+            tmp += 1
+            if tmp == num_of_jobs:
+                break
+        print("skipped")
+    save_ids_localy(to_prev_ids_lst)
     browser.quit()
 
 """ Working with Data Frames """
@@ -298,5 +325,5 @@ with webdriver.Chrome() as browser:
 df = pd.DataFrame(data)
 # save raw df locally before creating columns
 df.to_csv("my_data_raw.csv", index=False)
-df = df_to_email.clean_data(df, sortby_choice, given_origin)
-df_to_email.send_emails(df, email_to)
+# df = df_to_email.clean_data(df, sortby_choice, given_origin)
+# df_to_email.send_emails(df, email_to)
